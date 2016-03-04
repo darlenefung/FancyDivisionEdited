175400d1b56f55b007d94d4d1db483a419a00840
Darlene Fung <Darlene@Darlenes-MacBook-Pro.local>
2016-03-04 14:38:19 -0500

    changed 0 to int, added formatstring, added comments

--- FancyDivisionEdited.py
+++ FancyDivisionEdited.py
@@ -1,19 +1,19 @@
 # 2/4/16
-print("Hello, what is your name?")								
+print("Hello, what is your name?")   # asks the user what their name is						
 name = raw_input()
-print("Hello " + name)
+print("Hello " + name)			     # greets user
 
-print("Enter a dividend.")
+print("Enter a dividend.")			 # gets the dividend the user wants to use
 dividend = int(raw_input())
 
-print("Enter a divisor.")
+print("Enter a divisor.")			  # gets the divisor the user wants to use
 divisor = int(raw_input())
 
-answer = int(dividend / divisor)
-remainder = float(dividend % divisor)
+answer = int(dividend / divisor)	  # gets the answer with no remainder
+remainder = float(dividend % divisor) # gets the remainder of the two numbers
  
 if remainder != 0:
-	print("{} divided by {} is {} with a remainder of {}".format(divisor, dividend, answer, remainder))
+	print("{} divided by {} is {} with a remainder of {}".format(divisor, dividend, answer, remainder)) # if the remainder is no 0, the program prints the answer with the remainder 
 
 if remainder == 0:
-	print("{} divided by {} is {}".format(divisor, dividend, answer))
+	print("{} divided by {} is {}".format(divisor, dividend, answer)) # if the remainder is 0, it means there is no remainder and the program prints the answer without the remainder clause
