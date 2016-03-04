3412d0382d636a7fee13e8808749b30fc893f2c3
Darlene Fung <Darlene@Darlenes-MacBook-Pro.local>
2016-03-04 14:28:31 -0500

    changed name to mark as edited program

---
+++ FancyDivisionEdited.py
@@ -0,0 +1,19 @@
+# 2/4/16
+print("Hello, what is your name?")								
+name = raw_input()
+print("Hello " + name)
+
+print("Enter a dividend.")
+dividend = int(raw_input())
+
+print("Enter a divisor.")
+divisor = int(raw_input())
+
+answer = int(dividend / divisor)
+remainder = float(dividend % divisor)
+ 
+if remainder != 0:
+	print("{} divided by {} is {} with a remainder of {}".format(divisor, dividend, answer, remainder))
+
+if remainder == 0:
+	print("{} divided by {} is {}".format(divisor, dividend, answer))
